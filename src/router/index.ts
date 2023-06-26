cd2986c093bd2205209393e70db245e94d7260e6
Hoang Cong Phan <hoangcp219@gmail.com>
2023-06-26 21:18:06 +0700

    :sparkles: Feat: implement router vue

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,34 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 
 import HomeView from '../views/HomeView.vue'
 
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/:catchAll(.*)',
+    redirect: { name: 'home' }
+  },
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (About.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import('../views/AboutView.vue'),
+    meta: {
+      layout: 'LayoutProfile'
+    }
+  }
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
   strict: true,
+<<<<<<< Updated upstream
   scrollBehavior: () => ({ left: 0, top: 0 }),
   routes: [
     {
@@ -29,6 +53,9 @@ const router = createRouter({
       }
     }
   ]
+=======
+  scrollBehavior: () => ({ left: 0, top: 0 })
+>>>>>>> Stashed changes
 })
 
 export default router
